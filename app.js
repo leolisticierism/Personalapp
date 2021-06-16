07f2c4e6a0b57095f9ec463f5685c96156ac6dfe
leolisticierism <85957254+leolisticierism@users.noreply.github.com>
2021-06-16 12:31:42 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,171 +1,159 @@
-/*
-  app.js -- This creates an Express webserver
-*/
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const layouts = require("express-ejs-layouts");
+//const auth = require('./config/auth.js');
 
-// First we load in all of the packages we need for the server...
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const session = require("express-session");
-//const bodyParser = require("body-parser");
-const axios = require("axios");
-var debug = require("debug")("personalapp:server");
 
-// Now we create the server
+const mongoose = require( 'mongoose' );
+//mongoose.connect( `mongodb+srv://${auth.atlasAuth.username}:${auth.atlasAuth.password}@cluster0-yjamu.mongodb.net/authdemo?retryWrites=true&w=majority`);
+mongoose.connect( 'mongodb://localhost/authDemo');
+//const mongoDB_URI = process.env.MONGODB_URI
+//mongoose.connect(mongoDB_URI)
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("we are connected!!!")
+});
+
+const authRouter = require('./routes/authentication');
+const isLoggedIn = authRouter.isLoggedIn
+const loggingRouter = require('./routes/logging');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const toDoRouter = require('./routes/todo');
+const toDoAjaxRouter = require('./routes/todoAjax');
+
+
+
 const app = express();
 
-// Here we specify that we will be using EJS as our view engine
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-// Here we process the requests so they are easy to handle
+app.use(cors());
+app.use(layouts);
+
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Here we specify that static files will be in the public folder
-app.use(express.static(path.join(__dirname, "public")));
+app.use(authRouter)
+app.use(loggingRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-// Here we enable session handling ..
-app.use(
-  session({
-    secret: "zzbbya789fds89snana789sdfa",
-    resave: false,
-    saveUninitialized: false
-  })
-);
+app.use('/todo',toDoRouter);
+app.use('/todoAjax',toDoAjaxRouter);
 
-//app.use(bodyParser.urlencoded({ extended: false }));
-
-// This is an example of middleware
-// where we look at a request and process it!
-app.use(function(req, res, next) {
-  //console.log("about to look for routes!!! "+new Date())
-  console.log(`${req.method} ${req.url}`);
-  //console.dir(req.headers)
-  next();
-});
-
-// here we start handling routes
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-app.get("/demo",
-        function (req, res){res.render("demo");});
-
-app.get("/about", (request, response) => {
-  response.render("about");
-});
-
-app.get("/form", (request,response) => {
-  response.render("form")
+const myLogger = (req,res,next) => {
+  console.log('inside the testing route!')
+  next()
+}
+app.get('/testing/',
+  myLogger,
+  isLoggedIn,
+  (req,res) => {
+    res.render('testing')
 })
 
-app.get("/dataDemo", (request,response) => {
-  response.locals.name="Tim Hickey"
-  response.locals.vals =[1,2,3,4,5]
-  response.locals.people =[
-    {'name':'Tim','age':65},
-    {'name':'Yas','age':29}]
-  response.render("dataDemo")
+app.get('/testing2/', (req,res) => {
+  res.render('testing2')
 })
 
-app.post("/showformdata", (request,response) => {
-  response.json(request.body)
-})
-
-// Here is where we will explore using forms!
-
-
-
-// this example shows how to get the current US covid data
-// and send it back to the browser in raw JSON form, see
-// https://covidtracking.com/data/api
-// for all of the kinds of data you can get
-app.get("/c19",
-  async (req,res,next) => {
-    try {
-      const url = "https://covidtracking.com/api/v1/us/current.json"
-      const result = await axios.get(url)
-      res.json(result.data)
-    } catch(error){
-      next(error)
+app.get('/profiles',
+    isLoggedIn,
+    async (req,res,next) => {
+      try {
+        res.locals.profiles = await User.find({})
+        res.render('profiles')
+      }
+      catch(e){
+        next(e)
+      }
     }
-})
+  )
 
-// this shows how to use an API to get recipes
-// http://www.recipepuppy.com/about/api/
-// the example here finds omelet recipes with onions and garlic
-app.get("/omelet",
-  async (req,res,next) => {
-    try {
-      const url = "http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3"
-      const result = await axios.get(url)
-      res.json(result.data)
-    } catch(error){
-      next(error)
+app.use('/publicprofile/:userId',
+    async (req,res,next) => {
+      try {
+        let userId = req.params.userId
+        res.locals.profile = await User.findOne({_id:userId})
+        res.render('publicprofile')
+      }
+      catch(e){
+        console.log("Error in /profile/userId:")
+        next(e)
+      }
     }
+)
+
+
+app.get('/profile',
+    isLoggedIn,
+    (req,res) => {
+      res.render('profile')
+    })
+
+app.get('/editProfile',
+    isLoggedIn,
+    (req,res) => res.render('editProfile'))
+
+app.post('/editProfile',
+    isLoggedIn,
+    async (req,res,next) => {
+      try {
+        let username = req.body.username
+        let age = req.body.age
+        req.user.username = username
+        req.user.age = age
+        req.user.imageURL = req.body.imageURL
+        await req.user.save()
+        res.redirect('/profile')
+      } catch (error) {
+        next(error)
+      }
+
+    })
+
+
+app.use('/data',(req,res) => {
+  res.json([{a:1,b:2},{a:5,b:3}]);
 })
 
-// Don't change anything below here ...
+const User = require('./models/User');
 
-// here we catch 404 errors and forward to error handler
+app.get("/test",async (req,res,next) => {
+  try{
+    const u = await User.find({})
+    console.log("found u "+u)
+  }catch(e){
+    next(e)
+  }
+
+})
+
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// this processes any errors generated by the previous routes
+// error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
-
-//Here we set the port to use
-const port = "5000";
-app.set("port", port);
-
-// and now we startup the server listening on that port
-const http = require("http");
-const server = http.createServer(app);
-
-server.listen(port);
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-  debug("Listening on " + bind);
-}
-
-function onError(error) {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-
-  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-server.on("error", onError);
-
-server.on("listening", onListening);
 
 module.exports = app;
